49ba333aaeb2a4388171e9b4631d32e70f305e93
NetKBs <abijose535@gmail.com>
2024-04-20 17:21:06 -0400

    feat(/): first commit

---
+++ src/main/java/com/local/blockchain/Blockchain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.local.blockchain;
+
+/**
+ *
+ * @author avian
+ */
+public class Blockchain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
