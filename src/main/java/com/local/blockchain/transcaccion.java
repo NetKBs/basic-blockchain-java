261abf53d3f01ca0f68ab662ffa431b77857b06d
NetKBs <abijose535@gmail.com>
2024-04-20 17:40:46 -0400

    feat(): add attributes, constructor and getters for transaccion class

--- src/main/java/com/local/blockchain/transcaccion.java
+++ src/main/java/com/local/blockchain/transcaccion.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.local.blockchain;
 
-/**
- *
- * @author avian
- */
 public class transcaccion {
+    private int id;
+    private String emisor; 
+    private String receptor;
+    private float cantidad;
+    private int firmaDigital;
+    private String timestamp;
+
+    public transcaccion(String emisor, String receptor, float cantidad, int firmaDigital) {
+        this.emisor = emisor;
+        this.receptor = receptor;
+        this.cantidad = cantidad;
+        this.firmaDigital = firmaDigital;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmisor() {
+        return emisor;
+    }
+
+    public String getReceptor() {
+        return receptor;
+    }
+
+    public float getCantidad() {
+        return cantidad;
+    }
+
+    public int getFirmaDigital() {
+        return firmaDigital;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+    
+    
     
 }
