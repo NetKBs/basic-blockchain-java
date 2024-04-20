301d38bb5cc21da9d9f6fc8643270258fee9f9b3
NetKBs <abijose535@gmail.com>
2024-04-20 18:08:18 -0400

    feat(): crear modelo basico de clase Nodo

---
+++ src/main/java/com/local/blockchain/Nodo.java
@@ -0,0 +1,14 @@
+
+package com.local.blockchain;
+
+public class Nodo {
+    private static int id;
+    private Cartera cartera;
+    private Cadena cadena;
+    
+    public Nodo(){
+        this.id += 1;
+    }
+    
+    
+}
