a614144015f07022ec87e14cc40f8c4321d2ce44
NetKBs <abijose535@gmail.com>
2024-04-20 17:35:13 -0400

    feat(): create transaction class model

--- src/main/java/com/local/blockchain/Blockchain.java
+++ src/main/java/com/local/blockchain/Blockchain.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.local.blockchain;
 
-/**
- *
- * @author avian
- */
 public class Blockchain {
 
     public static void main(String[] args) {
