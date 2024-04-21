c1e3c0584bdb92208d3ede89f21ccbc30ba7334a
NetKBs <abijose535@gmail.com>
2024-04-21 16:43:23 -0400

    feat(): crear pk y sk de la cartera con ecdsa

--- src/main/java/com/local/blockchain/Cartera.java
+++ src/main/java/com/local/blockchain/Cartera.java
@@ -1,18 +1,46 @@
 
 package com.local.blockchain;
 
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Cartera {
     
-    private String clavePublica;
-    private String clavePrivada;
+    private PublicKey clavePublica;
+    private PrivateKey clavePrivada;
     private String direccion;
     private float fondos;
     
     public Cartera() {
-        this.clavePublica = "ClavePublica";
-        this.clavePrivada = "ClavePrivada";
+        this.generarClaves();
         this.direccion = "Direccion";
         this.fondos = 0;
+    }
+    
+    public void generarClaves() {
+        // Usando algoritmo ECDSA
+        KeyPairGenerator keyPairGenerator = null;
+        
+        try {
+            keyPairGenerator = KeyPairGenerator.getInstance("EC"); // algoritmo EC
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Cartera.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256r1"); // Parámetros de la curva
+        
+        try {
+            keyPairGenerator.initialize(ecSpec);
+        } catch (InvalidAlgorithmParameterException ex) {
+            Logger.getLogger(Cartera.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        this.clavePrivada = keyPair.getPrivate();
+        this.clavePublica = keyPair.getPublic();
+       
     }
     
     public void setDireccion(String direccion) {
@@ -23,11 +51,11 @@ public class Cartera {
         return direccion;
     }
 
-    public String getClavePublica() {
+    public PublicKey getClavePublica() {
         return clavePublica;
     }
 
-    public String getClavePrivada() {
+    public PrivateKey getClavePrivada() {
         return clavePrivada;
     }
     
