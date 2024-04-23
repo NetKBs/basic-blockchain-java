191f3ebe8da0cf7e651658e62415c8aa67a235ed
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-04-23 11:27:34 -0400

    merge

--- src/main/java/com/local/blockchain/Nodo.java
+++ src/main/java/com/local/blockchain/Nodo.java
@@ -1,22 +1,16 @@
-
 package com.local.blockchain;
 
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.security.Signature;
-import java.security.SignatureException;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import com.local.exepciones.FirmaException;
 
 public class Nodo {
     private static int id;
     private Cartera cartera;
     private Cadena cadena;
 
-    public Nodo(){
+    public Nodo() {
         this.id += 1;
         this.cartera = new Cartera();
     }
@@ -24,57 +18,44 @@ public class Nodo {
     public static int getId() {
         return id;
     }
-    
-    
-    public void transaccion(String receptor, float monto) {
-        GestorTransacciones gestor = new GestorTransacciones();        
+
+    public Transaccion crearTransaccion(String receptor, float monto) throws FirmaException {
+        String direccion = cartera.getDireccion();
         Instant timestamp = Instant.now();
-        String datos = cartera.getDireccion() + receptor + monto + timestamp;
-        byte[] firma = generarFirma(datos);
-  
-        if (firma != null) {
-            Transaccion transaccion = new Transaccion(cartera.getDireccion(), receptor, monto, timestamp, firma);
-            gestor.procesarTransaccion(cartera.getClavePublica(), transaccion);
-            
-        }
-        return;
+
+        String datos = direccion + receptor + monto + timestamp;
+        byte[] firma = this.generarFirma(datos);
+
+        Transaccion transaccion = new Transaccion(direccion, receptor, monto, timestamp, firma);
+
+        return transaccion;
     }
-    
-    private byte[] generarFirma(String datos) {
-        // Crear un objeto Signature e inicializarlo para la firma
+
+    private byte[] generarFirma(String datos) throws FirmaException {
         Signature signature = null;
+
         try {
-            
             signature = Signature.getInstance("SHA256withECDSA");
             signature.initSign(cartera.getClavePrivada());
             byte[] bytes = datos.getBytes(StandardCharsets.UTF_8);
             signature.update(bytes);
-            byte[] digitalSignature = signature.sign();  
-            return digitalSignature;
-            
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(GestorTransacciones.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InvalidKeyException ex) {
-            Logger.getLogger(GestorTransacciones.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SignatureException ex) {
-            Logger.getLogger(GestorTransacciones.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
-    }
-    
+            byte[] digitalSignature = signature.sign();
 
-    
+            return digitalSignature;
+        } catch (Exception ex) {
+            throw new FirmaException("No se pudo generar la firma");
+        }
+    }
+
     public String getDireccionCartera() {
         return cartera.getDireccion();
     }
-    
+
     public Cadena getCadena() {
         return cadena;
     }
-    
+
     public Cartera getCartera() {
         return cartera;
     }
-    
-    
 }
---
+++ src/main/java/com/local/exepciones/FirmaException.java
@@ -0,0 +1,7 @@
+package com.local.exepciones;
+
+public class FirmaException extends Exception{
+    public FirmaException(String errorMessage){
+        super(errorMessage);
+    }
+}
